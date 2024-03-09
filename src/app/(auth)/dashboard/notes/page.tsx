3251afbecc46bf14e4794d9d0bf570374e4cacc1
kojiado <kojiado@outlook.com>
2024-03-09 20:51:48 +0100

    Note

--- src/app/(auth)/dashboard/notes/page.tsx
+++ src/app/(auth)/dashboard/notes/page.tsx
@@ -3,7 +3,9 @@ import Note from "./components/Note"
 export default function Page() {
     return (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
-            <Note/>
+            <Note 
+                id="2323"
+            />
         </div>
     )
 }
