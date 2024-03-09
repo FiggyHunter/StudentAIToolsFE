123ca8e847749aa07218d1f76f26f8810211bb80
FiggyHunter <leonardo.roi3@gmail.com>
2024-03-10 00:13:59 +0100

    changes before new features

--- src/app/(auth)/dashboard/layout.tsx
+++ src/app/(auth)/dashboard/layout.tsx
@@ -1,39 +1,36 @@
-"use client"
+"use client";
 
 import type { Metadata } from "next";
-import Sidebar from "./(components)/Sidebar"
+import Sidebar from "./(components)/Sidebar";
 import Topbar from "./(components)/Topbar";
 import "@/styles/globals.css";
 import { useState } from "react";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
 
-const [isSidebarOpen, setSidebarOpen] = useState(false);
+  const toggleSidebar = (isOpen: boolean) => {
+    setSidebarOpen(isOpen);
+  };
 
-const toggleSidebar = (isOpen: boolean) => {
-  setSidebarOpen(isOpen);
-};
+  const pathname = usePathname();
 
-const pathname = usePathname();
+  const pathArray = pathname.split("/").filter((item) => item !== "");
 
-const pathArray = pathname.split("/").filter(item => item !== '');
-
-return (
+  return (
     <html lang="en">
       <body className="bg-primaryTint1 w-full h-full flex">
-        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
+        <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
         <div className="flex flex-col w-full lg:w-[calc(100%-350px)] lg:ml-[350px]">
-          <Topbar routeItems={pathArray} toggleSidebar={toggleSidebar}/>
-          <div className="p-7">
-            {children}
-          </div>
+          <Topbar routeItems={pathArray} toggleSidebar={toggleSidebar} />
+          <div className="p-7">{children}</div>
         </div>
-        </body>
+      </body>
     </html>
   );
 }
--- src/app/(auth)/dashboard/notes/[noteId]/page.tsx
+++ src/app/(auth)/dashboard/notes/[noteId]/page.tsx
@@ -2,6 +2,8 @@
 
 import { fetchNoteById } from "@/apiHandlers/notes";
 import { useEffect, useState } from "react";
+import Link from "next/link";
+import GoBack from "@/components/shared/GoBack";
 
 export default function Page({ params }: { params: { noteId: string } }) {
   const [note, setNote] = useState();
@@ -9,9 +11,10 @@ export default function Page({ params }: { params: { noteId: string } }) {
     fetchNoteById(params.noteId, setNote);
   }, []);
   return (
-    <section>
-      <h1>{note?.title}</h1>
-      <p>{note?.text}</p>
-    </section>
+    <div className="flex flex-col gap-4">
+      <GoBack />
+      <h1 className="text-2xl text-white">{note?.title}</h1>
+      <p className="text-text">{note?.text}</p>
+    </div>
   );
 }
