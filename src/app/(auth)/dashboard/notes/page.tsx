2c488081df4889dad8a9dad70f870639c0a56c32
FiggyHunter <leonardo.roi3@gmail.com>
2024-03-09 22:25:17 +0100

    focus fire at gun

--- src/app/(auth)/dashboard/notes/page.tsx
+++ src/app/(auth)/dashboard/notes/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
     fetchAllNotes(setNotes);
   }, []);
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-10">
       <h1 className="text-4xl text-white">Notes</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full">
         {notes?.map((note, index) => (
