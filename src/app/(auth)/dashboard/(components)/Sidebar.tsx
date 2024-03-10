dcd75db0aec3b58128c91ad48c3cea14804ed8a7
kojiado <kojiado@outlook.com>
2024-03-10 01:12:15 +0100

    Sideabar

--- src/app/(auth)/dashboard/(components)/Sidebar.tsx
+++ src/app/(auth)/dashboard/(components)/Sidebar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 import { SummaCraft, SummaPlus, SummaTasks, SyntaxSense, SpeecifyMe, Notes, SummarizedText } from "../icons"
-import { useState } from "react";
+import { LightLogo } from "../icons"
 
 interface SidebarProps {
   isSidebarOpen: boolean;
@@ -24,7 +24,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, toggleSidebar }) => {
     <div className={`flex flex-col gap-10 justify-between fixed top-0 w-[350px] box-border bg-primaryColor p-7 overflow-auto h-[100vh] transition-all duration-700 lg:left-0 ${isSidebarOpen ? 'left-0' : 'left-[-350px]'} z-40`}>
         <div className="flex flex-col gap-10">
           <div className="flex items-center w-full justify-between">
-            <span className="uppercase text-white font-bold text-[36px]">Logo</span>
+                {LightLogo}
             <svg onClick={closeSidebar} className="lg:hidden cursor-pointer" width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M23.8072 1.19507L1.52148 23.4808" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
               <path d="M1.52148 1.19507L23.8072 23.4808" stroke="white" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
